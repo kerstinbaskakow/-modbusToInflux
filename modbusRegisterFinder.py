406e4815517e55afb9445ac9e5b49ac49e2c00e4
Kerstin Baskakow <kerstin.baskakow@web.de>
2020-10-06 17:40:18 +0200

    Made code more efficiently added new register values with calculation of physical values

--- modbusRegisterFinder.py
+++ modbusRegisterFinder.py
@@ -7,11 +7,43 @@ Created on Sat Oct  3 12:15:49 2020
 """
 
 from pyModbusTCP.client import ModbusClient
+import pandas as pd
 
 modbusclient=ModbusClient(host="192.168.2.108",port=502)
 modbusclient.open()
 
+measurement_items = {40067:'pv',
+                     40071:'hausverbrauch',
+                     40082:'soc',
+                     40069:'Batterieleistung',
+                     40073:'Netz',
+                     40081:'AutarkieUndEigenverbrauch'}
+name = None
+
+print('Batterie: positiv: Laden, Negativ:entladen')
+print('Netz: positiv: Bezug, Negativ: Einspeisen')
+df = pd.DataFrame(columns=['name','rawValue','physValue','rawBin'])
+
 for x in range(40000,40100,1):
+
     regs = modbusclient.read_holding_registers(x)[0]
-    print(x,': ',regs)
+    print(x,regs)
+    rawBin= bin(regs).split('0b')[1]
+    name = measurement_items.get(x)
+    if name=='AutarkieUndEigenverbrauch':
+        autarkie=int(bin(regs).split('0b')[1][0:-8],2)
+        eigenverbrauch = int(bin(regs).split('0b')[1][-8:],2)
+        df.loc[x+20000] = ['Autarkie',regs,autarkie,rawBin]
+        df.loc[x] = ['Eigenverbrauch',regs,eigenverbrauch,rawBin]
+    elif name:
+        if regs<32768:
+            df.loc[x] = [name,regs,regs,rawBin]
+        else:
+            df.loc[x] = [name,regs,regs-65535,rawBin]
+    #else:
+    #    df.loc[x] = [name,regs,None,rawBin]
+#df.to_csv('modbusRegisterList.csv')            
+print(df)
+
+modbusclient.close()
 
--- modbusToInflux.py
+++ modbusToInflux.py
@@ -8,28 +8,72 @@ starttime = time.time()
 influxclient = InfluxDBClient(host='localhost', port=8086)
 influxclient.switch_database('iobroker')
 
-
+# start modbus client
 modbusclient=ModbusClient(host="192.168.2.108",port=502)
 modbusclient.open()
 
-measurement_items = [{'pv':40067},{'hausverbrauch':40071},{'soc':40082}]
-
-#while True:
+#definition of to be meassured items by register number
+measurement_items = {40067:'pv',
+                     40071:'hausverbrauch',
+                     40082:'soc',
+                     40069:'batterieleistung',
+                     40073:'netzleistung',
+                     40081:'AutarkieUndEigenverbrauch'
+                     }
+#define storing time as timebase in influx and grafana
 mytime = datetime.datetime.utcnow()
-influxdata=[]
-for item in measurement_items:
-    for key,reg in item.items():
-        influxdata.append({
-        "measurement": key,
-        "time":mytime,
-        "fields": {
-            "value": modbusclient.read_holding_registers(reg)[0],
-                    }
-    })
 
+#define measurement points, each point is a dictionary appended to a list of 
+#points
+influxdata=[]
+for key,reg in measurement_items.items():
+    regs = modbusclient.read_holding_registers(key)[0]
+    #one register contains two unit8 values, therefore it is converted to 
+    #bin and split after 8 bits, the reformated to dec
+    if reg=='AutarkieUndEigenverbrauch':
+        autarkie=int(bin(regs).split('0b')[1][0:-8],2)
+        influxdata.append({
+            "measurement": "autarkie",
+            "time":mytime,
+            "fields": {
+                "value": autarkie,
+                        }
+            })
+        eigenverbrauch = int(bin(regs).split('0b')[1][-8:],2)
+        influxdata.append({
+            "measurement": "eigenverbrauch",
+            "time":mytime,
+            "fields": {
+                "value": eigenverbrauch,
+                        }
+            })
+    #chech signed  ints for leading bit to be 1 or 0, 1 means negativ values
+    elif reg:
+        #positive values
+        if regs<32768:
+            influxdata.append({
+            "measurement": reg,
+            "time":mytime,
+            "fields": {
+                "value": modbusclient.read_holding_registers(key)[0],
+                        }
+            })
+        #negative values (substract 65535 for physical values)
+        else:
+            influxdata.append({
+            "measurement": reg,
+            "time":mytime,
+            "fields": {
+                "value": modbusclient.read_holding_registers(key)[0]-65535,
+                        }
+            })
+    
+
+print(influxdata)
+
+#write points to influx database
 influxclient.write_points(influxdata, database='iobroker', time_precision='ms', batch_size=10000, protocol='json')
 
-#    time.sleep(60.0 - ((time.time() - starttime) % 60.0))
 
 modbusclient.close()
 
